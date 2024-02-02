cea7729a4f714e46a614e082f4661ad699bddfb2
chee-zaram <ecokeke21@gmail.com>
2024-02-02 13:20:26 -0500

    Fix linting errors

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -130,7 +130,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
-    
+
     def test_get_account(self):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
